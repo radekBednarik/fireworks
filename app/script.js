b8ba4291f3bf4863c5af69a180a31c1eaa7c2e58
radekBednarik <radek.bednarik@tesena.com>
2020-12-25 19:27:12 +0100

    _

--- app/script.js
+++ app/script.js
@@ -1,8 +1,37 @@
+let firework;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800, WEBGL);
+  firework = new Firework(100, 100);
 }
 
 function draw() {
   background(220);
-  ellipse(50, 50, 80, 80);
+  firework.display();
+}
+
+class Firework {
+  constructor(posX, posY) {
+    this.coordinates = [
+      [-10, 10],
+      [0, 35],
+      [10, 10],
+      [35, 0],
+      [10, -8],
+      [0, -35],
+      [-10, -8],
+      [-35, 0],
+    ];
+    this.posX = posX;
+    this.posY = posY;
+    this.display = () => {
+      fill(237, 34, 93);
+      noStroke();
+      beginShape();
+      this.coordinates.forEach((item) => {
+        vertex(this.posX + item[0], this.posY + item[1]);
+      });
+      endShape();
+    };
+  }
 }
